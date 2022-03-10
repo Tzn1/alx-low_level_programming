e4610072bbb58d0e2559d2cd3b5cb7eaf887cc2b
Tzn1 <somethingcreative844@gmail.com>
2022-03-10 12:28:51 -0800

    code with printf instead of puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with a proper grammar, but the outcome is a pieceof art,\n");
+	return (0);
+}
