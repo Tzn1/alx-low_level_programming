696730be3b24fd435abf7bb54aaad8fddf58d015
Tzn1 <somethingcreative844@gmail.com>
2022-03-11 14:45:28 -0800

    sigle digitd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- main block
+ * Description: prints all single digit numbers of base 10 using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch=0; ch>10; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
