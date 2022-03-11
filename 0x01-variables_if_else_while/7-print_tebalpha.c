b5a3b971725310a89b24b9f5a5e4ae637b7ed664
Tzn1 <somethingcreative844@gmail.com>
2022-03-11 15:07:26 -0800

    reverses the alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- main block
+ * Description: prints the alpahabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch='a'; char l='z';
+
+	while (ch < l)
+	{
+		putchar(l);
+			ch++;
+	}
+	putchar('\n');
+	return (0);
+}
