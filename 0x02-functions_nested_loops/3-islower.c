95b17376d960e8440728df63e77954b3f5d89f8e
Tzn1 <somethingcreative844@gmail.com>
2022-03-15 13:06:04 -0700

    cheack for lower and uppercase alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
  * main- enrty point
- * Description: a function that checks for lowercase character
- * Return: Always 0;
+ * _islower - a function that checks for lowercase character
+ * Return: Always 0
  */
 
 int _islower(int c)
-{ if (c >= 97 && c <= 122)
-	return (1);
-else 
-	return (0);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
